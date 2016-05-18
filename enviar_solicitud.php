e3255d210bc49918f7b6273999a4d73641ddb416
mcalderonf <maxcalderon30@gmail.com>
2016-05-18 13:16:19 -0300

    Create enviar_solicitud.php

---
+++ enviar_solicitud.php
@@ -0,0 +1,13 @@
+<?php
+
+require "../../config.php";// archivo que ayuda a subir los datos del formulario a BBDD
+include '/formulario_libros.php'; //se incluyen dentro del codigo diferentes funciones 
+error_reporting (E_ALL ^ E_WARNING);
+// var_dump ( $_FILES ); 
+// var_dump ( $_REQUEST );
+
+
+
+
+
+?>
